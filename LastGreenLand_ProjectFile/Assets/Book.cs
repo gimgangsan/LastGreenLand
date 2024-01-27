9cb10c20d4c955d6ce83846f19b4ac01d2f0ea97
gimgangsan <75228703+gimgangsan@users.noreply.github.com>
2024-01-28 08:15:24 +0900

    이제 4개 상태창 모두 껏다켰다 가능

--- LastGreenLand_ProjectFile/Assets/Book.cs
+++ LastGreenLand_ProjectFile/Assets/Book.cs
@@ -25,8 +25,11 @@ public class Book : MonoBehaviour
         StatusBookmark.OnClick.AddListener(OpenCover);
         StatusBookmark.OnClick.AddListener(ShowStatus);
         ItemBookmark.OnClick.AddListener(OpenCover);
+        ItemBookmark.OnClick.AddListener (ShowItem);
         CraftBookmark.OnClick.AddListener(OpenCover);
+        CraftBookmark.OnClick.AddListener(ShowCraft);
         LogBookmark.OnClick.AddListener(OpenCover);
+        LogBookmark.OnClick.AddListener(ShowLog);
     }
 
     public void CloseCover()
@@ -64,6 +67,8 @@ public class Book : MonoBehaviour
     public void ShowStatus()
     {
         StatusPage.SetActive(true);
+        currentBookmark = StatusBookmark;
+
         StatusBookmark.OnClick.RemoveListener(ShowStatus);
         StatusBookmark.OnClick.AddListener(CloseStatus);
         StatusBookmark.OnClick.AddListener(CloseCover);
@@ -72,15 +77,74 @@ public class Book : MonoBehaviour
     public void CloseStatus()
     {
         StatusPage.SetActive(false);
+        currentBookmark = null;
+
         StatusBookmark.OnClick.RemoveListener(CloseStatus);
         StatusBookmark.OnClick.AddListener(ShowStatus);
+    }
+
+    public void ShowItem()
+    {
+        ItemPage.SetActive(true);
+        currentBookmark = ItemBookmark;
+
+        ItemBookmark.OnClick.RemoveListener(ShowItem);
+        ItemBookmark.OnClick.AddListener(CloseItem);
+        ItemBookmark.OnClick.AddListener(CloseCover);
+    }
+
+    public void CloseItem()
+    {
+        ItemPage.SetActive(false);
+        currentBookmark = null;
+
+        ItemBookmark.OnClick.RemoveListener(CloseItem);
+        ItemBookmark.OnClick.AddListener(ShowItem);
+    }
+
+    public void ShowCraft()
+    {
+        CraftPage.SetActive(true);
+        currentBookmark = CraftBookmark;
+
+        CraftBookmark.OnClick.RemoveListener(ShowCraft);
+        CraftBookmark.OnClick.AddListener(CloseCraft);
+        CraftBookmark.OnClick.AddListener(CloseCover);
+    }
+
+    public void CloseCraft()
+    {
+        CraftPage.SetActive(false);
+        currentBookmark = null;
+
+        CraftBookmark.OnClick.RemoveListener(CloseCraft);
+        CraftBookmark.OnClick.AddListener(ShowCraft);
+    }
+
+    public void ShowLog()
+    {
+        LogPage.SetActive(true);
+        currentBookmark = LogBookmark;
+
+        LogBookmark.OnClick.RemoveListener(ShowLog);
+        LogBookmark.OnClick.AddListener(CloseLog);
+        LogBookmark.OnClick.AddListener(CloseCover);
+    }
+
+    public void CloseLog()
+    {
+        LogPage.SetActive(false);
+        currentBookmark = null;
+
+        LogBookmark.OnClick.RemoveListener(CloseLog);
+        LogBookmark.OnClick.AddListener(ShowLog);
     }
 
     private void Update()
     {
         if(Input.GetKeyDown(KeyCode.Escape))
         {
-            CloseCover();
+            (currentBookmark)?.OnClick.Invoke();
         }
     }
 }
