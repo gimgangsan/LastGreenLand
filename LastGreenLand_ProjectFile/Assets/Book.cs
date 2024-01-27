8f71ef2658ad8f1b89ebd8734b2bad09d7b13ea3
gimgangsan <75228703+gimgangsan@users.noreply.github.com>
2024-01-28 07:58:57 +0900

    상태창 켰다 껐다가 하기

--- LastGreenLand_ProjectFile/Assets/Book.cs
+++ LastGreenLand_ProjectFile/Assets/Book.cs
@@ -4,15 +4,83 @@ using UnityEngine;
 
 public class Book : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private SpriteRenderer sprite;
+
+    [SerializeField] private Canvas coverText;
+
+    [SerializeField] private GameObject StatusPage;
+    [SerializeField] private GameObject ItemPage;
+    [SerializeField] private GameObject CraftPage;
+    [SerializeField] private GameObject LogPage;
+
+    [SerializeField] private ButtonInteraction StatusBookmark;
+    [SerializeField] private ButtonInteraction ItemBookmark;
+    [SerializeField] private ButtonInteraction CraftBookmark;
+    [SerializeField] private ButtonInteraction LogBookmark;
+    private ButtonInteraction currentBookmark;
+
+    private void Awake()
     {
-        
+        sprite = GetComponent<SpriteRenderer>();
+        StatusBookmark.OnClick.AddListener(OpenCover);
+        StatusBookmark.OnClick.AddListener(ShowStatus);
+        ItemBookmark.OnClick.AddListener(OpenCover);
+        CraftBookmark.OnClick.AddListener(OpenCover);
+        LogBookmark.OnClick.AddListener(OpenCover);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void CloseCover()
     {
-        
+        sprite.enabled = true;
+        coverText.enabled = true;
+
+        //make cover open next time
+        StatusBookmark.OnClick.AddListener(OpenCover);
+        ItemBookmark.OnClick.AddListener(OpenCover);
+        CraftBookmark.OnClick.AddListener(OpenCover);
+        LogBookmark.OnClick.AddListener(OpenCover);
+    }
+
+    public void OpenCover()
+    {
+        sprite.enabled = false;
+        coverText.enabled = false;
+
+        // make cover close next time
+        StatusBookmark.OnClick.RemoveListener(OpenCover);
+        ItemBookmark.OnClick.RemoveListener(OpenCover);
+        CraftBookmark.OnClick.RemoveListener(OpenCover);
+        LogBookmark.OnClick.RemoveListener(OpenCover);
+    }
+
+    public void HideAllPages()
+    {
+        StatusPage.SetActive(false);
+        ItemPage.SetActive(false);
+        CraftPage.SetActive(false);
+        LogPage.SetActive(false);
+    }
+
+    public void ShowStatus()
+    {
+        StatusPage.SetActive(true);
+        StatusBookmark.OnClick.RemoveListener(ShowStatus);
+        StatusBookmark.OnClick.AddListener(CloseStatus);
+        StatusBookmark.OnClick.AddListener(CloseCover);
+    }
+
+    public void CloseStatus()
+    {
+        StatusPage.SetActive(false);
+        StatusBookmark.OnClick.RemoveListener(CloseStatus);
+        StatusBookmark.OnClick.AddListener(ShowStatus);
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            CloseCover();
+        }
     }
 }
---
+++ LastGreenLand_ProjectFile/Assets/ButtonInteraction.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ButtonInteraction : MonoBehaviour
+{
+    private Color idleColor;
+    [SerializeField] private Color highlightColor;
+    [SerializeField] private Color holdColor;
+    [SerializeField] private Color clickColor;
+    private SpriteRenderer sprite;
+    public UnityEvent OnClick;
+
+    bool mouseOnButton;
+
+    private void Awake()
+    {
+        sprite = GetComponent<SpriteRenderer>();
+        idleColor = sprite.color;
+    }
+
+    private void OnMouseEnter()
+    {
+        sprite.color = highlightColor;
+        mouseOnButton = true;
+    }
+
+    private void OnMouseExit()
+    {
+        sprite.color = idleColor;
+        mouseOnButton = false;
+    }
+
+    private void OnMouseDown()
+    {
+        sprite.color = holdColor;
+    }
+
+    private void OnMouseUp()
+    {
+        sprite.color = (mouseOnButton)? highlightColor : idleColor;
+        if (mouseOnButton) ExecuteWhenClicked();
+    }
+
+    public virtual void ExecuteWhenClicked()
+    {
+        OnClick.Invoke();
+        Debug.Log("click");
+    }
+}
